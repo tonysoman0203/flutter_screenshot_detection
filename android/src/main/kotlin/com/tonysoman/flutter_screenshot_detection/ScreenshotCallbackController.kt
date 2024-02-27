42250221ad11c3483f488ee6d1d4e0cff98a9d08
Tony So <tony.so@price.com.hk>
2024-02-27 12:07:27 +0800

    - update

--- android/src/main/kotlin/com/tonysoman/flutter_screenshot_detection/ScreenshotCallbackController.kt
+++ android/src/main/kotlin/com/tonysoman/flutter_screenshot_detection/ScreenshotCallbackController.kt
@@ -1,40 +1,34 @@
 package com.tonysoman.flutter_screenshot_detection
 
-import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.Activity.ScreenCaptureCallback
-import android.app.AlertDialog
 import android.os.Build
-import android.util.Log
-import androidx.annotation.RequiresApi
 import io.flutter.plugin.common.EventChannel
-import io.flutter.plugin.common.MethodChannel
 
-@SuppressLint("LongLogTag")
 class ScreenshotCallbackController(val activity: Activity): EventChannel.StreamHandler  {
 
     private var screenshotCallback: ScreenCaptureCallback? = null
     private var eventSinks: EventChannel.EventSink? = null
 
     init {
-        Log.d("ScreenshotCallbackController", "init!")
+        Logger.shared.debug("ScreenshotCallbackController init!")
     }
 
     override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
-        Log.d("ScreenshotCallbackController", "onListen!")
+        Logger.shared.debug("ScreenshotCallbackController onListen!")
         eventSinks = events
         if (screenshotCallback == null) {
             screenshotCallback = ScreenCaptureCallback {
-                Log.d("ScreenshotCallbackController", "Take Screenshot")
-                AlertDialog.Builder(activity)
-                    .setMessage("You have taken a screenshot...")
-                    .setTitle("Warning")
-                    .show()
+                Logger.shared.debug("ScreenshotCallbackController Take Screenshot")
+//                AlertDialog.Builder(activity)
+//                    .setMessage("You have taken a screenshot...")
+//                    .setTitle("Warning")
+//                    .show()
                 eventSinks?.success(true)
             }
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
-                Log.d("ScreenshotCallbackController", "registerScreenCaptureCallback")
+                Logger.shared.debug("ScreenshotCallbackController registerScreenCaptureCallback")
                 activity.registerScreenCaptureCallback(activity.mainExecutor, screenshotCallback!!)
             }
         }
